df1de75bc7cfaac73a7caa8b384057d186afbe70
Ryan Lee <ryanlee91@gmail.com>
2017-11-18 18:48:09 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -42,7 +42,7 @@ class Question {
 
   // FIXME: Implement async / await
   static async Print(id){
-    const question = Question.Find(id)
+    const question = await Question.Find(id)
 
     console.log("\n")
     console.log(question.content)
